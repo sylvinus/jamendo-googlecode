ff886c0a1b0c0ec52f9d30e1464cfe6e85d48c56
sylvinus <sylvinus@eb8347f0-6a47-0410-9b48-bd58955dd0e0>
2008-04-10 02:50:28 +0000

    more tracks and removed masked albums

--- official/googlemusicgame/trackids.py
+++ official/googlemusicgame/trackids.py
@@ -1,6 +1,6 @@
 import simplejson
 
-#www.jamendo.com/get2/id/track/json/?n=2000&order=rating_desc
+#http://www.jamendo.com/get2/id/track/json/track_album/?n=10000&order=rating_desc
 
 #we could do that with a direct call to our API but still not sure how the import cache works.
 trackIds = simplejson.load(open("trackids.txt"))
